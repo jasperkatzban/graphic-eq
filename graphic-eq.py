55ec0d001ccc75f83a41f47e4a721d7c7ad3b366
Aydin O'Leary <aoleary@olin.edu>
2020-12-16 18:33:13 -0500

    Scaffold graphic-eq.py

--- graphic-eq.py
+++ graphic-eq.py
@@ -1,2 +1,22 @@
-@author: Aydin O'Leary (@zbwrm), Jasper Katzban (@jasperkatzban)
+# @author: Aydin O'Leary (@zbwrm), Jasper Katzban (@jasperkatzban)
 # Top-level graphic equalizer
+
+import pyaudio
+
+def fft(self, parameter_list):
+       """
+       docstring
+       """
+       pass
+
+def ifft(self, parameter_list):
+       """
+       docstring
+       """
+       pass
+
+def fitersample(self, parameter_list):
+       """
+       docstring
+       """
+       pass
