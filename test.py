1138ec914aa4e9baa89271a197e3a5ffd4f65f21
Aydin O'Leary <aoleary@olin.edu>
2020-12-17 22:57:28 -0500

    first pass at real-time playback

--- test.py
+++ test.py
@@ -9,15 +9,29 @@ import scipy.fftpack as ft
 import filters as f
 
 
-#%matplotlib tk
-
 CHUNK = 1024 * 2
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHANNELS = 1
 RATE = 44100
 
 p = pyaudio.PyAudio()
 
+# instantiate filter
+A = f.filtarray(preset='band')
+T = A.build()
+
+# funky function defs
+def callback(in_data, frame_count, time_info, status):   
+    global data, fft_data, fft_data_filtered, filt_data
+
+    data = struct.unpack(str(CHUNK) + 'h', in_stream.read(CHUNK))
+    fft_data = ft.rfft(data)
+    fft_data_filtered = np.multiply(fft_data,T)
+    filt_data = ft.irfft(fft_data_filtered)
+
+    return (filt_data, pyaudio.paContinue)
+
+# define input and output audio streams
 in_stream = p.open(format=FORMAT,
     channels=CHANNELS,
     rate=RATE,
@@ -31,10 +45,11 @@ out_stream = p.open(format=FORMAT,
     rate=RATE,
     input=False,
     output=True,
-    frames_per_buffer=CHUNK
+    frames_per_buffer=CHUNK,
+    stream_callback=callback
 )
 
-
+# set up active plotting
 plt.ion()
 fig1, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
 
@@ -50,28 +65,17 @@ filt_line, = ax3.plot(x, data_int16, 'r')
 fft_line, = ax2.semilogx(fft_x, np.random.rand(CHUNK))
 filt_fft_line, = ax4.semilogx(fft_x, np.random.rand(CHUNK), 'r')
 
-
 ax1.set_ylim([-2**15,(2**15)-1])
 ax3.set_ylim([-2**15,(2**15)-1])
 ax2.set_ylim(20, CHUNK * 2)
 ax4.set_ylim(20, CHUNK * 2)
 
-# cumbo jornburger
-A = f.filtarray(preset='band')
-T = A.build()
-# A.plotresponse()
 
-while True:
-    data = struct.unpack(str(CHUNK) + 'h', in_stream.read(CHUNK))
-    fft_data = ft.rfft(data)
-    fft_data_filtered = np.multiply(fft_data,T)
-    filt_data = ft.irfft(fft_data_filtered)
-
+# begin streaming
+out_stream.start_stream()
+while out_stream.is_active():
     fft_line.set_ydata(fft_data * 2 / (CHUNK))
     filt_fft_line.set_ydata(fft_data_filtered * 2 / (CHUNK))
-
-
-    
     # if time.monotonic() % .5 == 0:
     #     T = A.build()
 
@@ -80,5 +84,3 @@ while True:
 
     fig1.canvas.draw()
     fig1.canvas.flush_events()
-    # print(type(data))
-    # in_stream.write(data)
